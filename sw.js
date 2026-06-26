bd06672e2f2028de37add330556e50bc72829707
higalaxy121 <higalaxy121@gmail.com>
2026-06-26 11:19:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'eunho-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+// 설치: 모든 파일 캐시
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 활성화: 이전 캐시 삭제
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 요청: 캐시 우선, 없으면 네트워크
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
